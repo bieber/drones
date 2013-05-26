0dbcc3dca10a71ef427599da5502a6b974e0e147
Robert Bieber <robby@bieberphoto.com>
2013-05-26 01:11:06 -0700

    Making Progress on the VM
    I've added a debug function that will give me some idea of what's
    going on in the VM, a clock function with a handful of opcodes
    implemented, and functions to load data into memory.

--- vm/vm.go
+++ vm/vm.go
@@ -3,15 +3,44 @@
 package vm
 
 import (
-	"strings"
+	"encoding/binary"
 	"fmt"
+	"io"
+	"strings"
 )
 
-/*
- VM is an instance of the virtual machine.  It stores the VM's current state,
- and its methods can be used to mutate its state.  The buses are directly
- addressable, and allow the VM to communicate with external components.
-*/
+// Opcode represents the executable opcodes for the VM.
+type Opcode uint
+
+const (
+	_ Opcode = iota
+
+	// Does nothing for 1 clock cycle.
+	Nop
+
+	// Jumps to the argument address.
+	Jmp
+	// Jumps to the argument address only if a != 0.
+	Jmpc
+
+	// Loads a constant argument into a.
+	Ldc
+)
+
+// OpcodeNames returns a map of opcode values to their name.
+func OpcodeNames() map[Opcode]string {
+	return map[Opcode]string{
+		Nop:  "nop",
+		Jmp:  "jmp",
+		Jmpc: "jmpc",
+		Ldc:  "ldc",
+	}
+}
+
+// VM is an instance of the virtual machine.  It stores the VM's
+// current state, and its methods can be used to mutate its state.
+// The buses are directly addressable, and allow the VM to communicate
+// with external components.
 type VM struct {
 	// Registers
 	ip uint16
@@ -31,7 +60,8 @@ type VM struct {
 	Buses []uint16
 }
 
-// New allocates and initializes memory, buses and registers for a new VM.
+// New allocates and initializes memory, buses and registers for a new
+// VM.
 func New(memSize uint16, buses uint16) (vm *VM) {
 	vm = &VM{MemSize: memSize, sp: memSize - 1, bp: memSize - 1}
 	vm.mem = make([]uint16, memSize)
@@ -39,10 +69,84 @@ func New(memSize uint16, buses uint16) (vm *VM) {
 	return
 }
 
-// Debug returns a string that fully specifies the current state of the VM.
+// LoadMem copies a slice of uint16 into the VM's memory.
+func (vm *VM) LoadMem(memory []uint16) {
+	copy(vm.mem, memory)
+}
+
+// LoadOpcodes copies a slice of Opcodes into the VM's memory.
+func (vm *VM) LoadOpcodes(memory []Opcode) {
+	for i, v := range memory {
+		if i >= len(vm.mem) {
+			return
+		}
+		vm.mem[i] = uint16(v)
+	}
+}
+
+// LoadBinary reads a binary data file into the VM's memory.  The
+// binary file should begin with a single 16-bit value designating the
+// number of 16-bit words in the file, followed by that many values.
+// Byte-order is expected to be little-endian.
+func (vm *VM) LoadBinary(r io.Reader) error {
+	var size uint16
+	binary.Read(r, binary.LittleEndian, &size)
+	if int(size) < len(vm.mem) {
+		vm.mem = vm.mem[:size]
+	}
+	err := binary.Read(r, binary.LittleEndian, vm.mem)
+	vm.mem = vm.mem[:cap(vm.mem)]
+	return err
+}
+
+// Clock advances the VM by one clock cycle.  The opcode located at
+// the memory location at index ip will be executed with the opcode
+// located at memory location ip + 1 used as an argument.  See the
+// Opcode constants for more information.
+func (vm *VM) Clock() {
+	// If something goes wrong reset ip to 0 to simulate a machine
+	// reset.
+	defer func() {
+		if recover() != nil {
+			vm.ip = 0
+		}
+	}()
+
+	opcode := vm.mem[vm.ip]
+	arg := vm.mem[vm.ip+1]
+	vm.ip += 2
+	switch Opcode(opcode) {
+	case Nop:
+		// Do nothing
+
+	case Jmp:
+		vm.ip = arg
+	case Jmpc:
+		if vm.a != 0 {
+			vm.ip = arg
+		}
+
+	case Ldc:
+		vm.a = arg
+
+	default:
+		panic("vm: Invalid opcode")
+	}
+}
+
+// Debug returns a string that fully specifies the current state of
+// the VM.
 func (vm *VM) Debug() string {
-	titleLine := fmt.Sprintf("%7s %8s %8s %6s", "", "Signed", "Unsigned", "Hex")
+	titleLine := fmt.Sprintf(
+		"%6s  %6s %8s %8s %6s",
+		"",
+		"Opcode",
+		"Signed",
+		"Unsigned",
+		"Hex",
+	)
 	lines := []string{
+		"=================",
 		"=== Registers ===\n",
 		titleLine,
 		debugLine("ip", vm.ip),
@@ -53,18 +157,31 @@ func (vm *VM) Debug() string {
 		debugLine("p", vm.p),
 		debugLine("i", vm.i),
 		"\n=== Memory ===\n",
+		titleLine,
 	}
 	for i, v := range vm.mem {
 		lines = append(lines, debugLine(fmt.Sprintf("%#04x", i), v))
 	}
-	lines = append(lines, "\n=== Buses ===\n")
+	lines = append(lines, "\n=== Buses ===\n", titleLine)
 	for i, v := range vm.Buses {
 		lines = append(lines, debugLine(fmt.Sprintf("%#04x", i), v))
 	}
+	lines = append(lines, "\n")
 	return strings.Join(lines, "\n")
 }
 
 // Generates a line of the VM debug output for a given value and label
 func debugLine(label string, value uint16) string {
-	return fmt.Sprintf("%6s: %8d %8d %#04x", label, value, int16(value), value)
+	opcodeName, ok := OpcodeNames()[Opcode(value)]
+	if !ok {
+		opcodeName = "n/a"
+	}
+	return fmt.Sprintf(
+		"%6s: %6s %8d %8d %#04x",
+		label,
+		opcodeName,
+		value,
+		int16(value),
+		value,
+	)
 }
--- vmtest/vmtest.go
+++ vmtest/vmtest.go
@@ -4,9 +4,14 @@ package main
 import (
 	"fmt"
 	"github.com/bieber/drones/vm"
+	"bytes"
 )
 
 func main() {
-	v := vm.New(20, 10)
+	mem := []byte{5, 0, 4, 0, 5, 3, 6, 25, 78, 12, 3, 4}
+	v := vm.New(4, 5)
+	v.LoadBinary(bytes.NewReader(mem))
+	fmt.Println(v.Debug())
+	v.Clock()
 	fmt.Println(v.Debug())
 }
